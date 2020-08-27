69f985d3c337cbe2cabda4cf4669b133cb563439
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2020-08-27 18:12:25 +0530

    Command line parsing works

--- wagit/main.cpp
+++ wagit/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "argh.h"
 
-int main() {
+int main(int, char* argv[]) {
     std::cout << "Hello, World!" << std::endl;
+
+    argh::parser cmdl(argv);
+
+    if (cmdl[{ "-v", "--verbose" }])
+        std::cout << "Hello, verbosity!\n";
+
     return 0;
 }
